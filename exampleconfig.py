af7cf1ceece504958596bbeaf9a964be66b7de15
Devmeos <119659334+Devmeos@users.noreply.github.com>
2023-01-27 16:02:37 +0300

    Add files via upload

---
+++ exampleconfig.py
@@ -0,0 +1,7 @@
+@@ -12,5 +12,4 @@ class Development(Config):
+    SUDO_COMMAND_HAND_LER = "."
+    TZ = "Asia/Baghdad"
+
+# BabylonMeos
+# ملاحضه تمسح الكلام العربي وتضع ببدالة الكود 
+# BabylonMeos
