fe01f966ac45e53b6d73fdbf263a710b1f005921
Devmeos <119659334+Devmeos@users.noreply.github.com>
2023-01-31 17:00:18 +0300

    Delete exampleconfig.py

--- exampleconfig.py
+++
@@ -1,7 +0,0 @@
-@@ -12,5 +12,4 @@ class Development(Config):
-    SUDO_COMMAND_HAND_LER = "."
-    TZ = "Asia/Baghdad"
-
-# BabylonMeos
-# ملاحضه تمسح الكلام العربي وتضع ببدالة الكود 
-# BabylonMeos
